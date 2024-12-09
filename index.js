50678f7f4912555176b8b521f345ef4fef188da3
ikramuddin21 <shaonakhand2122@gmail.com>
2024-12-09 12:13:06 +0600

    some changes

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.VERCEL_PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilfiw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.VERCEL_DB_USER}:${process.env.VERCEL_DB_PASS}@cluster0.ilfiw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
